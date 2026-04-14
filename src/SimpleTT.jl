74c2a8c96f71ea10a83c4eb45111d1dac4c9a094
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2026-04-14 19:48:32 +0900

    feat: add pure Julia SimpleTT mpo contraction

--- src/SimpleTT.jl
+++ src/SimpleTT.jl
@@ -11,6 +11,131 @@ end
 
 Base.length(tt::TensorTrain) = length(tt.sitetensors)
 
+_getindex(x, indices) = ntuple(i -> x[indices[i]], length(indices))
+
+function _contract(
+    a::AbstractArray{T1,N1},
+    b::AbstractArray{T2,N2},
+    idx_a::NTuple{n1,Int},
+    idx_b::NTuple{n2,Int},
+) where {T1,T2,N1,N2,n1,n2}
+    length(idx_a) == length(idx_b) || error("length(idx_a) != length(idx_b)")
+    length(unique(idx_a)) == length(idx_a) || error("idx_a contains duplicate elements")
+    length(unique(idx_b)) == length(idx_b) || error("idx_b contains duplicate elements")
+    all(1 <= idx <= N1 for idx in idx_a) || error("idx_a contains elements out of range")
+    all(1 <= idx <= N2 for idx in idx_b) || error("idx_b contains elements out of range")
+
+    rest_idx_a = setdiff(1:N1, idx_a)
+    rest_idx_b = setdiff(1:N2, idx_b)
+
+    amat = reshape(
+        permutedims(a, (rest_idx_a..., idx_a...)),
+        prod(_getindex(size(a), rest_idx_a)),
+        prod(_getindex(size(a), idx_a)),
+    )
+    bmat = reshape(
+        permutedims(b, (idx_b..., rest_idx_b...)),
+        prod(_getindex(size(b), idx_b)),
+        prod(_getindex(size(b), rest_idx_b)),
+    )
+
+    return reshape(
+        amat * bmat,
+        _getindex(size(a), rest_idx_a)...,
+        _getindex(size(b), rest_idx_b)...,
+    )
+end
+
+function _contractsitetensors(a::Array{T,4}, b::Array{T,4})::Array{T,4} where {T}
+    ab::Array{T,6} = _contract(a, b, (3,), (2,))
+    abpermuted = permutedims(ab, (1, 4, 2, 5, 3, 6))
+    return reshape(
+        abpermuted,
+        size(a, 1) * size(b, 1),
+        size(a, 2),
+        size(b, 3),
+        size(a, 4) * size(b, 4),
+    )
+end
+
+function _check_mpo_contract_compatibility(a::TensorTrain{T,4}, b::TensorTrain{T,4}) where {T}
+    length(a) == length(b) || throw(ArgumentError("Tensor trains must have the same length."))
+    for n in 1:length(a)
+        size(a.sitetensors[n], 3) == size(b.sitetensors[n], 2) ||
+            error("Tensor trains must share the identical index at n=$(n)!")
+    end
+    return nothing
+end
+
+function contract_naive(
+    a::TensorTrain{T,4},
+    b::TensorTrain{T,4};
+    tolerance=0.0,
+    maxbonddim=typemax(Int),
+) where {T}
+    _check_mpo_contract_compatibility(a, b)
+    tt = TensorTrain{T,4}(_contractsitetensors.(a.sitetensors, b.sitetensors))
+    if tolerance > 0 || maxbonddim < typemax(Int)
+        compress!(tt, :SVD; tolerance=tolerance, maxbonddim=maxbonddim)
+    end
+    return tt
+end
+
+function contract_zipup(
+    a::TensorTrain{T,4},
+    b::TensorTrain{T,4};
+    tolerance=1e-12,
+    method::Symbol=:SVD,
+    maxbonddim::Int=typemax(Int),
+) where {T}
+    _check_mpo_contract_compatibility(a, b)
+    R::Array{T,3} = ones(T, 1, 1, 1)
+    sitetensors = Vector{Array{T,4}}(undef, length(a))
+
+    for n in 1:length(a)
+        RA = _contract(R, a.sitetensors[n], (2,), (1,))
+        C = permutedims(_contract(RA, b.sitetensors[n], (2, 4), (1, 2)), (1, 2, 4, 3, 5))
+        if n == length(a)
+            sitetensors[n] = reshape(C, size(C)[1:3]..., 1)
+            break
+        end
+
+        left, right, newbonddim = _factorize(
+            reshape(C, prod(size(C)[1:3]), prod(size(C)[4:5])),
+            method;
+            tolerance=tolerance,
+            maxbonddim=maxbonddim,
+        )
+        sitetensors[n] = reshape(left, size(C)[1:3]..., newbonddim)
+        R = reshape(right, newbonddim, size(C)[4:5]...)
+    end
+
+    return TensorTrain{T,4}(sitetensors)
+end
+
+"""
+    contract(a::TensorTrain{T,4}, b::TensorTrain{T,4}; algorithm=:naive, tolerance=1e-12,
+             maxbonddim=typemax(Int), kwargs...)
+
+Contract two MPO-like `SimpleTT.TensorTrain` objects.
+"""
+function contract(
+    a::TensorTrain{T,4},
+    b::TensorTrain{T,4};
+    algorithm::Symbol=:naive,
+    tolerance=1e-12,
+    maxbonddim::Int=typemax(Int),
+    kwargs...,
+) where {T}
+    if algorithm === :naive
+        return contract_naive(a, b; tolerance=tolerance, maxbonddim=maxbonddim)
+    elseif algorithm === :zipup
+        return contract_zipup(a, b; tolerance=tolerance, maxbonddim=maxbonddim, kwargs...)
+    else
+        throw(ArgumentError("Unknown algorithm $algorithm."))
+    end
+end
+
 function _factorize(
     A::AbstractMatrix{V},
     method::Symbol;
